e5b1441ac7172862380071e9018b475053ce8c90
yogesh <yogesh@celestialsys.com>
2024-08-06 13:32:26 +0530

    theming using mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useMode, ColorModeContext } from "./theme";
+import TopBar from './screens/globals/TopBar';
+
 function App() {
+  const [theme, colorMode] = useMode();
+ 
   return (
-    <div className="app">
-      Hello
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <TopBar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
---
+++ src/screens/globals/TopBar.tsx
@@ -0,0 +1,7 @@
+function TopBar() {
+    return(
+        <div>TopBar</div>
+    )
+};
+
+export default TopBar;
